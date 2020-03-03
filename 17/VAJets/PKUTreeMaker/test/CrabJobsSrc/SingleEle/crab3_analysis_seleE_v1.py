7d277c9bf2c10a9e9f343dffe5677b65fba89b96
JINGFFF <15827238926@163.com>
2020-03-03 18:04:03 +0800

    JEC and crab

---
+++ 17/VAJets/PKUTreeMaker/test/CrabJobsSrc/SingleEle/crab3_analysis_seleE_v1.py
@@ -0,0 +1,25 @@
+from WMCore.Configuration import Configuration
+config = Configuration()
+config.section_("General")
+config.General.requestName   = '94x_seleE_v1'
+config.General.transferLogs = True
+
+config.section_("JobType")
+config.JobType.pluginName  = 'Analysis'
+config.JobType.inputFiles =['Summer16_07Aug2017EF_V11_DATA_L1FastJet_AK4PFchs.txt','Summer16_07Aug2017EF_V11_DATA_L2Relative_AK4PFchs.txt','Summer16_07Aug2017EF_V11_DATA_L3Absolute_AK4PFchs.txt','Summer16_07Aug2017EF_V11_DATA_L2L3Residual_AK4PFchs.txt','Summer16_07Aug2017EF_V11_DATA_L1FastJet_AK4PFPuppi.txt','Summer16_07Aug2017EF_V11_DATA_L2Relative_AK4PFPuppi.txt','Summer16_07Aug2017EF_V11_DATA_L3Absolute_AK4PFPuppi.txt','Summer16_07Aug2017EF_V11_DATA_L2L3Residual_AK4PFPuppi.txt']
+
+config.JobType.psetName    = 'analysis_data_EF.py'
+config.JobType.allowUndistributedCMSSW = True
+
+config.section_("Data")
+#config.Data.outputPrimaryDataset = 'VBS_WGAMMA_94X'
+config.Data.inputDataset = '/SingleElectron/Run2016E-17Jul2018-v1/MINIAOD'
+config.Data.inputDBS = 'global'
+config.Data.splitting = 'LumiBased'
+config.Data.unitsPerJob = 40
+config.Data.lumiMask = 'Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON.txt'
+config.Data.publication = False
+config.Data.outputDatasetTag = '94x_seleE_v1'
+
+config.section_("Site")
+config.Site.storageSite = 'T2_CN_Beijing'
---
+++ 18/VAJets/PKUTreeMaker/test/CrabJobsSrc/submit.sh
@@ -0,0 +1,34 @@
+crab submit -c crab3_analysisWGToLNuG.py
+#crab submit -c ./Wcrab/crab3_analysisWA_private.py
+crab submit -c crab3_analysisST_t.py
+crab submit -c crab3_analysisST_tbar.py
+crab submit -c crab3_analysisST_s.py
+crab submit -c crab3_analysisST_tW_antitop.py
+crab submit -c crab3_analysisST_tW_top.py
+
+crab submit -c crab3_analysisWW_v1.py
+crab submit -c crab3_analysisWW_v2.py
+crab submit -c crab3_analysisWZ_v1.py
+crab submit -c crab3_analysisWZ_v2.py
+crab submit -c crab3_analysisZZ_v1.py
+crab submit -c crab3_analysisZZ_v2.py
+crab submit -c crab3_analysisZA.py
+
+crab submit -c crab3_analysisLNuAJJ_EWK.py
+crab submit -c crab3_analysisTTGJets_v1.py
+crab submit -c crab3_analysisTTGJets_v2.py
+
+#crab submit -c ./Wcrab/crab3_analysisZA_private.py
+#crab submit -c ./Wcrab/crab3_analysisW3Jets1.py
+#crab submit -c ./Wcrab/crab3_analysisW3Jets2.py
+crab submit -c crab3_analysisDY1.py
+crab submit -c crab3_analysisDY2.py
+crab submit -c crab3_analysisDY3.py
+crab submit -c crab3_analysisDY4.py
+crab submit -c crab3_analysisDY_v1.py
+crab submit -c crab3_analysisDY_v2.py
+
+crab submit -c crab3_analysisTT_v1.py
+crab submit -c crab3_analysisTT_v2.py
+crab submit -c crab3_analysisWJets_v1.py
+crab submit -c crab3_analysisWJets_v2.py
