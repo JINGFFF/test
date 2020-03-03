2a5addc1da04d384bf3e694bd6167f29948b95e8
JINGFFF <15827238926@163.com>
2020-03-03 15:28:53 +0800

    crab

---
+++ 16/VAJets/PKUTreeMaker/test/CrabJobs/mySubmit.sh
@@ -0,0 +1,24 @@
+crab submit -c ./myWcrab/crab3_analysisDY_v1.py
+crab submit -c ./myWcrab/crab3_analysisDY_v2.py
+
+crab submit -c ./myWcrab/crab3_analysisST_s.py
+crab submit -c ./myWcrab/crab3_analysisST_t.py
+crab submit -c ./myWcrab/crab3_analysisST_tW_antitop.py
+crab submit -c ./myWcrab/crab3_analysisST_tW_top.py
+crab submit -c ./myWcrab/crab3_analysisST_tbar.py
+
+crab submit -c ./myWcrab/crab3_analysisZA.py
+
+crab submit -c ./myWcrab/crab3_analysisWW_v1.py
+crab submit -c ./myWcrab/crab3_analysisWW_v2.py
+crab submit -c ./myWcrab/crab3_analysisWZ_v1.py
+crab submit -c ./myWcrab/crab3_analysisWZ_v2.py
+crab submit -c ./myWcrab/crab3_analysisZZ_v1.py
+crab submit -c ./myWcrab/crab3_analysisZZ_v2.py
+
+crab submit -c ./myWcrab/crab3_analysisTTGJets_v1.py
+crab submit -c ./myWcrab/crab3_analysisTTGJets_v2.py
+
+crab submit -c ./myWcrab/crab3_analysisWGToLNuG.py
+
+
